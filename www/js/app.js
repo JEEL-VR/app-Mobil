01c02624dd3d801d5824efc7422a7475e05bef20
evermine18 <edgarsanou@gmail.com>
2022-04-05 16:19:02 +0200

    Implementado, login, carga de cursos y tasks con la API de AWS

--- www/js/app.js
+++ www/js/app.js
@@ -54,62 +54,34 @@ $( document ).ready(function() {
     }
     function openCourse(cID){
         return function(){
-            $.getJSON("../jsonTest/get_course_details.json", function(data){
-                //Show user elements
-                $("#llista_elements").empty();
-                for (let element in data["elements"]){
-                    console.log(data["elements"][element]);
-                    let newElement = $('<a href="#" class="collection-item avatar"><i class="material-icons circle" style="background-color: #159A9C;">folder</i>'+data["elements"][element]["title"]+'</a>');
-                    $("#llista_elements").append(newElement);
-                }
-                //Show user tasks
-                $("#llista_tasks").empty();
-                for (let element in data["tasks"]){
-                    console.log(data["tasks"][element]);
-                    let newElement = $('<a href="#" class="collection-item avatar"><i class="material-icons circle" style="background-color: #159A9C;">format_list_bulleted</i>'+data["tasks"][element]["title"]+'</a>');
-                    $("#llista_tasks").append(newElement);
-                }
-                //Vr tasks
-                $("#llista_tasksvr").empty();
-                for (let element in data["vr_tasks"]){
-                    console.log(data["vr_tasks"][element]);
-                    let newElement = $('<a href="#" class="collection-item avatar"><i class="material-icons circle" style="background-color: #159A9C;">format_list_bulleted</i>'+data["vr_tasks"][element]["title"]+'</a>');
-                    newElement.click(getPin(data["vr_tasks"][element]["ID"]));
-                    $("#llista_tasksvr").append(newElement);
-                }
-                //Title and description edit to 2nd tab
-                $("#course_title").text(data["title"]);
-                $("#course_desc").text(data["description"]);
-            }).fail(function(){
-                console.log("An error has occurred.");
-            });
-            /*
+            console.log(cID);
             $.ajax({
                 method: "POST",
-                url: localStorage.getItem("api")+"/api/get_course_details",
-                data: {session_token:localStorage.getItem("sesion_token"),courseID:cID},
+                url: localStorage.getItem("api")+"/api/get_courses_detail",
+                headers: {"Authorization":"Token "+localStorage.getItem("sesion_token")},
+                data: JSON.stringify({courseID:cID}),
                 dataType: "json",
             }).done(function (data) {
+                console.log(data);
                 //Show user elements
                 $("#llista_elements").empty();
-                for (let element in data["elements"]){
-                    console.log(data["elements"][element]);
-                    let newElement = $('<a href="#" class="collection-item avatar"><i class="material-icons circle" style="background-color: #159A9C;">folder</i>'+data["elements"][element]["title"]+'</a>');
+                for (let element in data["course"]["elements"]){
+                    console.log(data["course"]["elements"][element]);
+                    let newElement = $('<a href="#" class="collection-item avatar"><i class="material-icons circle" style="background-color: #159A9C;">folder</i>'+data["course"]["elements"][element]+'</a>');
                     $("#llista_elements").append(newElement);
                 }
                 //Show user tasks
                 $("#llista_tasks").empty();
-                for (let element in data["tasks"]){
-                    console.log(data["tasks"][element]);
-                    let newElement = $('<a href="#" class="collection-item avatar"><i class="material-icons circle" style="background-color: #159A9C;">format_list_bulleted</i>'+data["tasks"][element]["title"]+'</a>');
+                for (let element in data["course"]["tasks"]){
+                    console.log(data["course"]["tasks"][element]);
+                    let newElement = $('<a href="#" class="collection-item avatar"><i class="material-icons circle" style="background-color: #159A9C;">format_list_bulleted</i>'+data["course"]["tasks"][element]+'</a>');
                     $("#llista_tasks").append(newElement);
                 }
                 //Vr tasks
                 $("#llista_tasksvr").empty();
-                for (let element in data["vr_tasks"]){
-                    console.log(data["vr_tasks"][element]);
-                    let newElement = $('<a href="#" class="collection-item avatar"><i class="material-icons circle" style="background-color: #159A9C;">format_list_bulleted</i>'+data["vr_tasks"][element]["title"]+'</a>');
-                    newElement.click(getPin(data["vr_tasks"][element]["ID"]));
+                for (let element in data["course"]["vr_tasks"]){
+                    console.log(data["course"]["vr_tasks"][element]);
+                    let newElement = $('<a href="#" class="collection-item avatar"><i class="material-icons circle" style="background-color: #159A9C;">format_list_bulleted</i>'+data["course"]["vr_tasks"][element]+'</a>');
                     $("#llista_tasksvr").append(newElement);
                 }
                 //Title and description edit to 2nd tab
@@ -123,7 +95,7 @@ $( document ).ready(function() {
                 $("#course_title").text("Error");
                 $("#course_desc").text("No se ha podido listar el curso");
             });
-            */
+        
             //Swipe to 2nd tab
             $('.tabs').tabs('select', "test-swipe-2");
         };
@@ -143,7 +115,7 @@ $( document ).ready(function() {
     $.ajax({
         method: "POST",
         url: localStorage.getItem("api")+"/api/get_courses",
-        headers: {"Authorization":"token "+localStorage.getItem("sesion_token")},
+        headers: {"Authorization":"Token "+localStorage.getItem("sesion_token")},
         dataType: "json",
     }).done(function (data) {
         //Show the curses
